9a23d61f4fa01097cd3895218da8ca6c981a1086
jhonatancastaneda <jhoncasta3d@hotmail.com>
2015-10-14 10:45:01 -0500

    3rd

--- script_01.R
+++ script_01.R
@@ -6,6 +6,8 @@ list.files()
 
 data <- read_sav("SPSS_Esmeraldas_Poblacion.sav")
 
+data <- poblacion
+
 dim(data)
 glimpse(data)
 
@@ -23,6 +25,30 @@ install.packages('DT',dependencies = TRUE)
 
 
 
+substring()
+dplyr
+
+data <- tbl_df(data)
+lapply(data, class)
+clases <- unlist (lapply(data, class))=="labelled"
+clases[clases==TRUE]
+clases[clases==FALSE]
+
+columnas <- which(unlist(lapply(data, class))=="numeric")
+subdata <- data[,columnas]
 
 
+
+est <- function(vector){
+  vari <- list()
+  vari$min <- min (vector,na.rm = TRUE)
+  vari$p5 <- quantile(vector,probs = c(0.05),na.rm = TRUE)
+  vari$mediana <- median(vector,na.rm = TRUE)
+  vari$p95 <- quantile(vector,probs = c(0.95),na.rm = TRUE)
+  vari$max <- max(vector,na.rm = TRUE)
+  return(unlist(vari))
+}
+  
+
+lapply(subdata, est)
 
